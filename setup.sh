8d97b3c7acffa765c4d8eb4dd28cb09a337a0f94
石井尚貴 <n.i.vandit0924@gmail.com>
2020-07-18 09:36:47 +0900

    vscode拡張機能の自動インストール

--- setup.sh
+++ setup.sh
@@ -5,3 +5,15 @@ echo "install homebrew"
 # execute homebrew
 echo "execute homebrew"
 brew bundle
+
+# set up visual studio code Extension
+code --install-extension codezombiech.gitignore
+code --install-extension donjayamanne.git-extension-pack
+code --install-extension golang.go
+code --install-extension jebbs.plantuml
+code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension ms-mssql.mssql
+code --install-extension tht13.python
+code --install-extension waderyan.gitblame
