c825b51d1090fdbbca2a805bba732465407c9101
石井尚貴 <n.i.vandit0924@gmail.com>
2020-07-18 10:46:55 +0900

    vscodeの拡張機能インストールを別ファイルに作成

---
+++ vscode.sh
@@ -0,0 +1,10 @@
+code --install-extension codezombiech.gitignore
+code --install-extension donjayamanne.git-extension-pack
+code --install-extension golang.go
+code --install-extension jebbs.plantuml
+code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-ceintl.vscode-language-pack-ja
+code --install-extension ms-mssql.mssql
+code --install-extension tht13.python
+code --install-extension waderyan.gitblame
