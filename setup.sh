150cec0bd0e0cbc33c90973bad28f0cbb344d65d
石井尚貴 <n.i.vandit0924@gmail.com>
2020-07-18 09:34:24 +0900

    homebrewのインストール&実行

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# install homebrew
+echo "install homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# execute homebrew
+echo "execute homebrew"
+brew bundle
