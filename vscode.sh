83f423497374c0167be7149d887ac2ca5f7d33fe
石井尚貴 <n.i.vandit0924@gmail.com>
2020-07-20 22:07:28 +0900

    vscode拡張機能追加

--- vscode.sh
+++ vscode.sh
@@ -7,5 +7,6 @@ code --install-extension mechatroner.rainbow-csv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-ceintl.vscode-language-pack-ja
 code --install-extension ms-mssql.mssql
+code --install-extension Remote-Containers
 code --install-extension tht13.python
 code --install-extension waderyan.gitblame
